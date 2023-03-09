4f69b6d8311f719f98f1d14a2bf2b8c96d0457ca
Awirut  Armeen <awirut091@gmail.com>
2023-03-09 18:58:49 +0000

    group by with loop

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,40 @@
 import streamlit as st
+import pandas as pd
 
-st.title('🎈 Awirut_App')
+st.title('🎈 Student554_App')
+st.write('Welcome to my App')
 
-st.write('Hello world!')
+url = st.sidebar.text_input('URL','')
+#st.info(f'URL of Data : {url}')
+
+
+
+if url:
+    st.header('Output')
+    st.info(f'URL of Data : {url}')
+    df = pd.read_csv(url)
+    st.write(df)
+    columns = df.columns 
+    size = columns.size
+    ca = [columns[size-1],columns[size-2]]
+
+
+    for i in range(0,size-2):
+      ca.insert(i, columns[i])
+
+    genre = st.radio(
+    "Group by :",(ca))
+    
+    if genre:
+        gb = df.groupby(genre).mean()
+        st.bar_chart(gb)
+
+else:
+    st.subheader('Enter URL!')
+
+#url = "https://raw.githubusercontent.com/dataprofessor/data/master/iris.csv"
+
+#df = pd.read_csv(url)
+
+#st.write(df)
+
